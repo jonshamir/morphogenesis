84aec32d37122690462561bbedd284064b856e3b
Jon Shamir <jon.shamir@gmail.com>
2023-05-27 17:46:54 +0300

    Fix loading

--- src/App.js
+++ src/App.js
@@ -1,121 +1,145 @@
 import { OrbitControls, useFBO } from "@react-three/drei";
 import { Canvas, useFrame, extend, createPortal } from "@react-three/fiber";
-import { PLYLoader } from 'three/examples/jsm/loaders/PLYLoader'
+import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader";
 import { useMemo, useState, useRef, useEffect } from "react";
 import * as THREE from "three";
-import { Vector3, Matrix4 } from "three";
-import './scene.css';
+import "./scene.css";
+import { useCallback } from "react";
 
-const material = new THREE.PointsMaterial({ size: 0.01, vertexColors: true });
+const material = new THREE.PointsMaterial({ size: 0.013, vertexColors: true });
 
-const PointCloud = ({ currFrame, setCurrFrame, frameCount, setLoadProgress, isPlaying }) => {
-  const loader = new PLYLoader();
-  const [geometry, setGeometry] = useState(null);
-  const pointsRef = useRef();
+const loader = new PLYLoader();
+
+const processGeometry = (g, frameIndex) => {
+  const p = g.attributes.position.array;
+  const c = g.attributes.color.array;
+
+  for (let i = 0; i < p.length; i += 3) {
+    // https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
+    const luminance = 0.2126 * c[i] + 0.7152 * c[i + 1] + 0.0722 * c[i + 2];
+
+    // Hide points that are too dark or too light
+    if (luminance > 0.6 || luminance < 0.05) p[i] = p[i + 1] = p[i + 2] = 0;
+    else if (frameIndex >= 80) {
+      // Temp fix for rotation in data
+      const v = new THREE.Vector3(p[i], p[i + 1], p[i + 2]);
+      v.applyEuler(new THREE.Euler(0, 0.1, 0));
+      p[i] = v.x;
+      p[i + 1] = v.y;
+      p[i + 2] = v.z;
+    }
+  }
+
+  return g;
+};
+
+const PointCloud = ({
+  currFrame,
+  setCurrFrame,
+  frameCount,
+  onProgressChanged,
+  isPlaying,
+}) => {
+  const [points, setPoints] = useState(null);
   const geometryCacheRef = useRef({});
   const filesLoadedRef = useRef(0);
-  
+
   useFrame((_, delta) => {
-    if (isPlaying)
-    {
-      setCurrFrame((currFrame + 1) % frameCount);
-    }
+    if (isPlaying) setCurrFrame(1 + ((currFrame + 1) % frameCount));
   });
 
   // Load frames
   useEffect(() => {
-    for (let i = 1; i < frameCount + 1; i++)
-    {
-      if (!geometryCacheRef.current[i]) {
-        loader.load(`/data/119-120_${i}.ply`, (g) => {
-          filesLoadedRef.current++;
-          setLoadProgress(filesLoadedRef.current / frameCount);
-            
-          const p = g.attributes.position.array;
-          const c = g.attributes.color.array;
-
-          for (let i = 0; i < p.length; i += 3) {
-            // https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
-            const luminance = (0.2126 * c[i] + 0.7152 * c[i+1] + 0.0722 * c[i+2])
-            
-            // Hide points that are too dark or too light
-            if (luminance > 0.6 || luminance < 0.05)
-              p[i] = p[i + 1] = p[i + 2] = 0;
-          }
-          geometryCacheRef.current[i] = g;
-        },
-        () => {}, // loading
-        error =>  console.error('Error loading .ply file:', error));
+    (async () => {
+      for (let i = 1; i < frameCount + 1; i++) {
+        if (!geometryCacheRef.current[i]) {
+          loader.load(
+            `/data/119-120_${i}.ply`,
+            (g) => {
+              filesLoadedRef.current++;
+              onProgressChanged(filesLoadedRef.current / frameCount);
+              geometryCacheRef.current[i] = processGeometry(g, i);
+            },
+            () => {},
+            (e) => console.error(e)
+          );
+          await new Promise((r) => setTimeout(r, 10));
+        }
       }
-    }
+    })();
   }, []);
 
   // Show frames
   useEffect(() => {
     if (geometryCacheRef.current[currFrame]) {
-      // Temp fix for rotation in data
-      if (currFrame >= 80) { 
-        const p = geometryCacheRef.current[currFrame].attributes.position.array;
-        const v = new Vector3(p[currFrame], p[currFrame+1], p[currFrame+2])
-        v.applyEuler(new THREE.Euler(0, 0.5, 0))
-        p[currFrame] = v.x;
-        p[currFrame+1] = v.y;
-        p[currFrame+2] = v.z;
-      }
-      setGeometry(geometryCacheRef.current[currFrame]);
-      pointsRef.current = new THREE.Points(geometry, material);
+      setPoints(
+        new THREE.Points(geometryCacheRef.current[currFrame], material)
+      );
+    } else {
+      console.error(`Missing frame ${currFrame}`);
+      setPoints(undefined);
+      new PLYLoader().load(`/data/119-120_${currFrame}.ply`, (g) => {
+        geometryCacheRef.current[currFrame] = processGeometry(g, currFrame);
+        setPoints(
+          new THREE.Points(geometryCacheRef.current[currFrame], material)
+        );
+      });
     }
-    else
-      setGeometry(undefined);
   }, [currFrame]);
 
-  return (
-    <points>
-      {geometry && <primitive object={pointsRef.current} />}
-    </points>
-  );
+  return <points>{points && <primitive object={points} />}</points>;
 };
 
-const Controls = () => {
-
-}
-
+const Controls = () => {};
 
 const App = () => {
   const frameCount = 243;
   const [loadProgress, setLoadProgress] = useState(0);
   const [currFrame, setCurrFrame] = useState(1);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [playSpeed, setPlaySpeed] = useState(1); 
+  const [playSpeed, setPlaySpeed] = useState(1);
 
   const handleSliderChange = (event) => {
     setCurrFrame(event.target.value);
   };
 
+  const handleProgressChange = useCallback(
+    (progress) => setLoadProgress(progress),
+    []
+  );
+
   return (
     <>
       <Canvas camera={{ position: [2, 2, 2] }}>
         <PointCloud
-        currFrame={currFrame}
-        setCurrFrame={setCurrFrame}
-        frameCount={frameCount}
-        setLoadProgress={setLoadProgress}
-        isPlaying={isPlaying} />
+          currFrame={currFrame}
+          setCurrFrame={setCurrFrame}
+          frameCount={frameCount}
+          onProgressChanged={handleProgressChange}
+          isPlaying={isPlaying}
+        />
         <OrbitControls />
-        <gridHelper  />
+        <gridHelper />
       </Canvas>
       <div className="overlay">
         <div className="controls">
-          <button className="material-icons" onClick={()=> setIsPlaying(!isPlaying)}>
-              {isPlaying ? 'pause' : 'play_arrow'}
+          <button
+            className="material-icons"
+            onClick={() => setIsPlaying(!isPlaying)}
+          >
+            {isPlaying ? "pause" : "play_arrow"}
           </button>
-          <button onClick={()=> setPlaySpeed(playSpeed > 8 ? 1 : 2*playSpeed)}>
+          <button
+            onClick={() => setPlaySpeed(playSpeed > 8 ? 1 : 2 * playSpeed)}
+          >
             {playSpeed}x
           </button>
-          {loadProgress < 0.98 && <span>
-            <span className="material-icons loader">sync</span>
-            { (loadProgress*100).toFixed(0)}%
-            </span>}
+          {loadProgress < 0.98 && (
+            <span>
+              <span className="material-icons loader">sync</span>
+              {(loadProgress * 100).toFixed(0)}%
+            </span>
+          )}
           <div className="fillSpace"></div>
           <span>{`${currFrame} / ${frameCount}`}</span>
         </div>
